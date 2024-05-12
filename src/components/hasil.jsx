c955f9e9fc3c9c5b21a66ebaaf54f865471b2232
irfancipinang <muhammadirfanm.2006@gmail.com>
2024-05-12 09:10:56 +0700

    new

--- src/components/hasil.jsx
+++ src/components/hasil.jsx
@@ -1,14 +1,26 @@
 import  { Component } from 'react'
-import Col from 'react-bootstrap/Col'
+import  Col from 'react-bootstrap/Col'
+import { ListGroup } from 'react-bootstrap'
 
 export default class hasil extends Component {
   render() {
+    const { keranjangs } = this.props
     return (
       <Col>
       <h5>
         <strong>Hasil</strong>
       </h5>
       <hr  style={{ width: '100%', border: '1px solid black' }}/>
+      {keranjangs.length !== 0 &&
+       <ListGroup variant="flush">
+          {keranjangs.map((menuKeranjang) => (
+             <ListGroup.Item>
+              {menuKeranjang.product.nama}
+             </ListGroup.Item>
+          ))}
+         
+       </ListGroup>
+      }
       </Col>
     )
   }
--- src/components/menus.jsx
+++ src/components/menus.jsx
@@ -1,10 +1,10 @@
 import { Col, Card } from 'react-bootstrap';
 import { numberWithCommas } from '../utils/number'
 
-const menus = ({menu}) => {
+const menus = ({menu, masukKeranjang}) => {
   return (
     <Col md={4} xs={6} className="mb-4">
-    <Card className="shadow">
+    <Card className="shadow" onClick={() => masukKeranjang(menu)}>
       <Card.Img variant="top" src={"public/assets/images/"+menu.category.nama.toLowerCase() +"/"+menu.gambar}/>
       <Card.Body>
         <Card.Title>{menu.nama}</Card.Title>
