10bdf518453c55c2b33fb371730e1cf7d78870f0
irfancipinang <muhammadirfanm.2006@gmail.com>
2024-05-14 11:38:25 +0700

    new

--- src/components/hasil.jsx
+++ src/components/hasil.jsx
@@ -1,19 +1,45 @@
 import  { Component } from 'react'
-import  Col from 'react-bootstrap/Col'
+import Col  from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import { ListGroup } from 'react-bootstrap'
+import { numberWithCommas } from '../utils/number';
+import Badge from 'react-bootstrap/Badge' 
 
-export default class hasil extends Component {
+export default class Hasil extends Component {
   render() {
     const { keranjangs } = this.props
+    console.log(keranjangs);
+    
     return (
       <Col>
       <h5>
         <strong>Hasil</strong>
       </h5>
       <hr  style={{ width: '100%', border: '1px solid black' }}/>
-      <ListGroup variant="flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-      </ListGroup>
+      {keranjangs &&
+         <ListGroup variant="flush">
+           {keranjangs.map((menuKeranjang) =>(
+            <ListGroup.Item>
+             <Row>
+              <Col xs={2}>
+              <h4>
+                <Badge pill variant="succes">
+                    {menuKeranjang.jumlah}
+                </Badge>
+              </h4>
+              </Col>
+              <Col>
+                 <h5>{menuKeranjang.product.nama}</h5>
+                 <p>Rp.{numberWithCommas(menuKeranjang.product.harga)}</p>
+              </Col>
+              <Col>
+                
+              </Col>
+             </Row>
+            </ListGroup.Item>
+           ))}
+       </ListGroup>
+      }
       </Col>
     )
   }
